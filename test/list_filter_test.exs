f0b4414105fa8dd9db423cd6049b2bcd440398ef
f.sousa <ng.dev.sousa@outlook.com>
2021-03-10 21:52:45 -0300

    Add tests

--- test/list_filter_test.exs
+++ test/list_filter_test.exs
@@ -1,8 +1,23 @@
 defmodule ListFilterTest do
   use ExUnit.Case
-  doctest ListFilter
 
-  test "greets the world" do
-    assert ListFilter.hello() == :world
+  describe "call/1" do
+    test "it should return the total of odd numbers in the provided list" do
+      list = ["1", "3", "6", "43", "banana", "6", "abc"]
+
+      result = ListFilter.call(list)
+      expected_result = 3
+
+      assert result == expected_result
+    end
+
+    test "it should return zero if invalid values are provided in the list" do
+      list = ["a", "b", "c", "2"]
+
+      result = ListFilter.call(list)
+      expected_result = 0
+
+      assert result == expected_result
+    end
   end
 end
